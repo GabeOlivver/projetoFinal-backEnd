5132c4fbaf8911918cf5c922499359ea369d3719
GabeOlivver <gabriel.oliveira294@fatec.sp.gov.br>
2024-06-29 12:13:37 -0300

    commit 3 - criação e preenchiento do banco de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO CONTATOS_DB(NAME, EMAIL, TELEFONE, TIPOCONTATO, PREFERENCIACONTATO, OBSERVACOES)
+VALUES('Gabriel', 'gabriel.oliveira@gmail.com', '11999999991', 'Academico', 'Telefone', 'eu')
+
+INSERT INTO CONTATOS_DB(NAME, EMAIL, TELEFONE, TIPOCONTATO, PREFERENCIACONTATO, OBSERVACOES)
+VALUES('Maria Silvia', 'maria.silvia@gmail.com', '11999999992', 'pessoal', 'Telefone', 'esposa')
+
+INSERT INTO CONTATOS_DB(NAME, EMAIL, TELEFONE, TIPOCONTATO, PREFERENCIACONTATO, OBSERVACOES)
+VALUES('Leonardo Kakiuchi', 'leonardo.kakiuchi@gmail.com', '11999999993', 'Profissional', 'Email', 'Clonador de cartão')
